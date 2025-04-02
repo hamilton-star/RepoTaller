3c4174b4a1275fd8f2e4f5697e514be8f5f3c0b9
JeanFranco <EstudianteUC@UCUXA60135.continental.edu.pe>
2025-04-01 19:42:06 -0500

    VO2-subiendoCarpetas

---
+++ Datos/Conexion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+
+namespace Datos
+{
+    public class Conexion
+    {
+        //declarar la cadena de conexion
+        static string cadena = "Server=localhost;Database=TallerMecanico;Integrated Security=true";
+        SqlConnection conexion = new SqlConnection(cadena);
+
+    }
+}
